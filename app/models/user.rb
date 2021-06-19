5c1450600df418a59f5d12e79f879dc97d7ca6cf
shun0328 <84302143+shun0328@users.noreply.github.com>
2021-06-19 22:59:18 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true,
+            format: {
+              with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/,
+              message: "is invalid. Input full-width katakana characters"
+            }
+  validates :first_name_kana, presence: true,
+            format: {
+              with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/,
+              message: "is invalid. Input full-width katakana characters"
+            }
+  validates :birthday, presence: true
+  validates :password, presence: true,
+            format: {
+              with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+              message: "is invalid. Include both letters and numbers"
+            }
+end
